629cdd9d21a54d1d7a87da8d7f01d074b2f9ce21
theTd <i@thetd.me>
2024-06-29 21:16:41 +0800

    PrimaryThreadCaller::asyncCall; snapshot 1.6.1

--- src/main/java/com/mineclay/tclite/PrimaryThreadCaller.java
+++ src/main/java/com/mineclay/tclite/PrimaryThreadCaller.java
@@ -5,27 +5,25 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 public abstract class PrimaryThreadCaller<T> {
     final JavaPlugin plugin;
+    final Executor executor;
 
     public PrimaryThreadCaller(JavaPlugin plugin) {
         this.plugin = plugin;
+        executor = (t) -> Bukkit.getScheduler().runTask(plugin, t);
     }
 
     protected abstract T syncCall();
 
     public T call() throws ExecutionException, InterruptedException {
         if (Bukkit.isPrimaryThread()) return syncCall();
+        return callAsync().get();
+    }
 
-        CompletableFuture<T> f = new CompletableFuture<>();
-        Bukkit.getScheduler().runTask(plugin, () -> {
-            try {
-                f.complete(syncCall());
-            } catch (Exception e) {
-                f.completeExceptionally(e);
-            }
-        });
-        return f.get();
+    public CompletableFuture<T> callAsync() {
+        return CompletableFuture.supplyAsync(this::syncCall, executor);
     }
 }
